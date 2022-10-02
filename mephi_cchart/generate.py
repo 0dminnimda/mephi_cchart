8c490f3b9f058cb79265329ae7962ffec4c94047
0dminnimda <0dminnimda@gmail.com>
2022-10-03 01:48:50 +0300

    generate.py: refactor a bit, add make_final_html() and render_pdf()

--- mephi_cchart/generate.py
+++ mephi_cchart/generate.py
@@ -5,10 +5,27 @@ from pathlib import Path
 
 os.chdir(str(Path(__file__).absolute().parent))
 
-inp = "./source/index.html"
-out = "./index.pdf"
-args = shlex.split(
-    f"ChromeHtmlToPdf/ChromeHtmlToPdfConsole.exe --input {inp} --output {out}"
-)
-p = subprocess.call(args)
-print(p)
+from public_data import repl
+
+
+def make_final_html(template: str, repl: dict, final: str) -> None:
+    text = Path(template).read_text("utf-8")
+
+    for name, value in repl.items():
+        text = text.replace("{" + name + "}", str(value))
+
+    Path(final).write_text(text, "utf-8")
+
+
+def render_pdf(inp, out):
+    args = shlex.split(
+        f"ChromeHtmlToPdf/ChromeHtmlToPdfConsole.exe --input {inp} --output {out}"
+    )
+    return subprocess.call(args)
+
+
+template = "./source/template.html"
+final = "./source/final.html"
+
+make_final_html(template, repl, final)
+print(render_pdf(final, final[:-4] + "pdf"))
