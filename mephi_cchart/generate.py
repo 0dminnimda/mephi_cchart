373ba288215043b8b1af0cf2c0b77de9ed3fe467
0dminnimda <0dminnimda@gmail.com>
2022-10-10 04:13:23 +0300

    generate.py: additionally render_terminal_png and fix imports

--- mephi_cchart/generate.py
+++ mephi_cchart/generate.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 
+from html2pdf import render_pdf
 from public_data import repl
+from terminal import render_terminal_png
 
 
-def make_final_html(template: str, repl: dict, final: str) -> None:
+def make_final_html(template: str, terminal: str, repl: dict, final: str) -> None:
     text = Path(template).read_text("utf-8")
 
     for name, value in repl.items():
@@ -11,12 +13,17 @@ def make_final_html(template: str, repl: dict, final: str) -> None:
 
     Path(final).write_text(text, "utf-8")
 
+    render_terminal_png(
+        terminal, repl["SCREENSHOTS"], margin=30, line_offset=6, font="Consola"
+    )
+
 
 if __name__ == "__main__":
     import fix_dir
 
     template = "./source/template.html"
     final = "./source/final.html"
+    image = "./source/terminal.png"
 
-    make_final_html(template, repl, final)
-    print(render_pdf(final, final[:-4] + "pdf"))
+    make_final_html(template, image, repl, final)
+    print(render_pdf(final))
