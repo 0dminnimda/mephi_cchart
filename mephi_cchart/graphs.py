2a59d4bc722456707d6c4bc8edf7e7fc6e5c0205
0dminnimda <0dminnimda@gmail.com>
2022-10-10 04:23:37 +0300

    graphs.py: use if __name__ and use public_data

--- mephi_cchart/graphs.py
+++ mephi_cchart/graphs.py
@@ -1,9 +1,6 @@
-import os
 from pathlib import Path
 
 import requests
-
-os.chdir(str(Path(__file__).absolute().parent))
 
 
 # possible types: https://github.com/yuzutech/kroki/blob/c60c44066ac38378be802c44fd5cca2cb54f530d/server/src/main/java/io/kroki/server/Server.java#L105
@@ -22,32 +19,13 @@ def save_graph(source: str, type: str, path: str) -> None:
     file.write_bytes(render_graph(source, type, file.suffix[1:]))
 
 
-g = """
-flowchart RL
-   subgraph SG0 [" "]
-      direction TB
-      n0_0["maximize(n)"] --> n0_1["size = ceil(log10(n))"]
-      n0_1 --> n0_2["sorted = 0"]
-      n0_2 --> n0_3["sorted = 1"]
-      n0_3 --> n0_4["offset = 1"]
-      n0_4 --> n0_5{{"i = 0; i < size - 1; i++"}}
-      n0_5 --> n0_13{"sorted != 1"}
-      n0_13 --> |true| n0_3
-      n0_5 --> n0_6["digit1 = n / (offset*10) % 10"]
-      n0_6 --> n0_7["digit2 = n / (offset   ) % 10"]
-      n0_7 --> n0_8{"digit1 > digit2"}
-      n0_8 --> |true| n0_9["n = n - digit1 * offset * 10 + digit2 * offset * 10"]
-      n0_9 --> n0_10["n = n - digit2 * offset      + digit1 * offset     "]
-      n0_10 --> n0_11["sorted = 0"]
-      n0_11 --> n0_12["offset *= 10"]
-      n0_8 --> |false| n0_12
-      n0_12 --> n0_5
-       
-   end
-"""
-# render_graph(g, "mermaid", "svg")
+if __name__ == "__main__":
+    import fix_dir
+    # from public_data import repl
 
-gg = """
+    # render_graph(repl["MERMAID-CODE"], "mermaid", "svg")
+
+    gg = """
 blockdiag admin {
   n0_0 [label = "maximize(n)"];
   n0_1 [label = "size = ceil(log10(n))"];
@@ -80,5 +58,5 @@ blockdiag admin {
   n0_8 -> n0_12  [label = "false"];
   // n0_12 -> n0_5;
 }
-"""
-save_graph(gg, "blockdiag", "t.svg")
+    """
+    save_graph(gg, "blockdiag", "t.svg")
