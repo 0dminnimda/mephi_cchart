a4a304d9e599121f54946912ec3d71febc589a5f
0dminnimda <0dminnimda@gmail.com>
2022-10-10 07:21:17 +0300

    graphs.py: add run_n_record()

--- mephi_cchart/generate.py
+++ mephi_cchart/generate.py
@@ -1,17 +1,20 @@
+import os
 from pathlib import Path
 
 from html2pdf import render_pdf
 from public_data import repl
 from terminal import render_terminal_png
+from tester import compile_program, run_program
 
 
-def make_final_html(template: str, terminal: str, repl: dict, final: str) -> None:
-    text = Path(template).read_text("utf-8")
+def run_n_record(filename: str, terminal: str) -> str:
+    exe = compile_program(filename)
 
-    for name, value in repl.items():
-        text = text.replace("{" + name + "}", str(value))
+    runs = []
+    os.system(exe)
 
-    Path(final).write_text(text, "utf-8")
+    result = run_program(exe, "100")
+    print(result.stdout.lstrip("\n"))
 
     render_terminal_png(
         terminal,
@@ -22,6 +25,17 @@ def make_final_html(template: str, terminal: str, repl: dict, final: str) -> Non
         min_line_length=30,
     )
 
+    return "user: ".join(runs)
+
+
+def make_final_html(template: str, repl: dict, final: str) -> None:
+    text = Path(template).read_text("utf-8")
+
+    for name, value in repl.items():
+        text = text.replace("{" + name + "}", str(value))
+
+    Path(final).write_text(text, "utf-8")
+
 
 if __name__ == "__main__":
     import fix_dir
@@ -29,6 +43,8 @@ if __name__ == "__main__":
     template = "./source/template.html"
     final = "./source/final.html"
     image = "./source/terminal.png"
+    name = "myprogram.c"
 
-    make_final_html(template, image, repl, final)
+    repl["TEST-EXAMPLES"] = run_n_record(name, image)
+    make_final_html(template, repl, final)
     print(render_pdf(final))
