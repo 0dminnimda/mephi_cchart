bd28447e16b71cea29b9dbf71050a898767052b1
0dminnimda <0dminnimda@gmail.com>
2022-10-13 08:06:42 +0300

    generate.py: add add_code(), and comment some things out

--- mephi_cchart/generate.py
+++ mephi_cchart/generate.py
@@ -1,3 +1,4 @@
+import html
 import os
 from pathlib import Path
 
@@ -6,26 +7,35 @@ from public_data import repl
 from terminal import render_terminal_png
 from tester import compile_program, run_program
 
+# def run_n_record(filename: str, terminal: str) -> str:
+#     exe = compile_program(filename)
 
-def run_n_record(filename: str, terminal: str) -> str:
-    exe = compile_program(filename)
+#     runs = []
+#     os.system(exe)
 
-    runs = []
-    os.system(exe)
+#     result = run_program(exe, "100")
+#     print(result.stdout.lstrip("\n"))
 
-    result = run_program(exe, "100")
-    print(result.stdout.lstrip("\n"))
+#     render_terminal_png(
+#         terminal,
+#         repl["SCREENSHOTS"],
+#         margin=30,
+#         line_offset=6,
+#         font="Consola",
+#         min_line_length=30,
+#     )
 
-    render_terminal_png(
-        terminal,
-        repl["SCREENSHOTS"],
-        margin=30,
-        line_offset=6,
-        font="Consola",
-        min_line_length=30,
-    )
+#     return "user: ".join(runs)
 
-    return "user: ".join(runs)
+
+def add_code(path: str) -> None:
+    text = Path(path).read_text()
+    lines = text.splitlines()
+    max_len = len(str(len(lines)))
+    lines = [
+        str(i) + " " * (max_len - len(str(i)) + 2) + ln for i, ln in enumerate(lines)
+    ]
+    repl["C-CODE"] = html.escape("\n".join(lines))
 
 
 def make_final_html(template: str, repl: dict, final: str) -> None:
@@ -45,6 +55,7 @@ if __name__ == "__main__":
     image = "./source/terminal.png"
     name = "myprogram.c"
 
-    repl["TEST-EXAMPLES"] = run_n_record(name, image)
+    add_code(name)
+    # repl["TEST-EXAMPLES"] = run_n_record(name, image)
     make_final_html(template, repl, final)
     print(render_pdf(final))
