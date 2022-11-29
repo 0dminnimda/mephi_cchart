819c3ac95be0013532b894bd6ae0d21583371f38
0dminnimda <0dminnimda@gmail.com>
2022-11-30 02:59:35 +0300

    generate.py: generate PROGRAMS - make things automated

--- mephi_cchart/generate.py
+++ mephi_cchart/generate.py
@@ -28,14 +28,24 @@ from tester import compile_program, run_program
 #     return "user: ".join(runs)
 
 
-def add_code(path: str) -> None:
-    text = Path(path).read_text()
-    lines = text.splitlines()
-    max_len = len(str(len(lines)))
-    lines = [
-        str(i) + " " * (max_len - len(str(i)) + 2) + ln for i, ln in enumerate(lines)
-    ]
-    repl["C-CODE"] = html.escape("\n".join(lines))
+def add_code(key: str, *files: str) -> None:
+    repl[key] = ""
+    template = repl[key + "-TEMPLATE"]
+    for ind, file in enumerate(files):
+        path = Path(file)
+        text = path.read_text()
+        lines = text.splitlines()
+        max_len = len(str(len(lines)))
+        lines = [
+            str(i) + " " * (max_len - len(str(i)) + 2) + ln
+            for i, ln in enumerate(lines)
+        ]
+        repl[key] += template.format(
+            number=ind + 1,
+            filename=path.stem,
+            filepath=path.name,
+            code=html.escape("\n".join(lines)),
+        )
 
 
 def make_final_html(template: str, repl: dict, final: str) -> None:
@@ -47,15 +57,20 @@ def make_final_html(template: str, repl: dict, final: str) -> None:
     Path(final).write_text(text, "utf-8")
 
 
+def filter_files(directory: str, *filters: str) -> list[str]:
+    glob = Path(directory).glob
+    return [str(file) for filter in filters for file in glob(filter)]
+
+
 if __name__ == "__main__":
     import fix_dir
 
     template = "./source/template.html"
     final = "./source/final.html"
     image = "./source/terminal.png"
-    name = "myprogram.c"
+    lab_folder = "../../mephi_labs/lab" + str(repl["LAB-ID"])
 
-    add_code(name)
+    add_code("PROGRAMS", *filter_files(lab_folder, "*.h", "*.c"))
     # repl["TEST-EXAMPLES"] = run_n_record(name, image)
     make_final_html(template, repl, final)
     print(render_pdf(final))
