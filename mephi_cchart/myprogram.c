00e0d3ac2b13d75c3a2a9f230e2906612773d3dc
0dminnimda <0dminnimda@gmail.com>
2022-11-29 19:55:30 +0300

    Update myprogram.c

--- mephi_cchart/myprogram.c
+++ mephi_cchart/myprogram.c
@@ -1,33 +1,57 @@
 #include <stdio.h>
 #include <math.h>
 
-int maximize(int n) {
-    int size = ceil(log10(n));
-    int sorted = 0;
-    while (sorted != 1) {
-        sorted = 1;
-        int offset = 1;
-        for (int i = 0; i < size - 1; i++) {
-            int digit1 = n / (offset*10) % 10;
-            int digit2 = n / (offset   ) % 10;
-            if (digit1 > digit2) {
-                n = n - digit1 * offset * 10 + digit2 * offset * 10;
-                n = n - digit2 * offset      + digit1 * offset     ;
-                sorted = 0;
-            }
-            offset *= 10;
-        }
+double root_iter(double a, double n, int num_of_iter)
+{
+    double y = a;
+    for (int i = 0; i < num_of_iter; i++)
+    {
+        y = ((n - 1) * y + a / pow(y, n - 1)) / n;
     }
-    return n;
+    return y;
 }
 
-int main() {
-    printf("Hello! Input one integer\n");
-    int n = 0;
-    scanf("%d", &n);
-    if (n > 0)
-        printf("Minimal digit combination: %d (leading zeros are not shown)\n", maximize(n));
-    else
-        printf("It's not an integer ;(\nSorry, but you'll have to enter an integer next time!\n");
-    return 0;
+int guard(int scanf_result)
+{
+    if (scanf_result == EOF)
+        return -1;
+    int result = (scanf_result <= 0);
+    char c;
+    while (((c = getchar()) != '\n') && (c != EOF))
+    {
+        if (c == EOF)
+            return -1;
+        result = 1;
+    }
+    return result;
+}
+
+#define SCAN(...)                                     \
+    if ((guard_res = guard(scanf(__VA_ARGS__))) == 1) \
+    {                                                 \
+        printf("Faulty input. Try again!\n\n");       \
+        continue;                                     \
+    }                                                 \
+    else if (guard_res == -1)                         \
+    {                                                 \
+        printf("Fatal error: EOF\n\n");               \
+        return -1;                                    \
+    }
+
+int main()
+{
+    while (1)
+    {
+        int guard_res = 0;
+        printf("Hello! Input two integers: base of the root and number\n");
+        double a = 0, n = 0;
+        SCAN("%lf %lf", &n, &a);
+        printf("Input number of iterations\n");
+        int third = 0;
+        SCAN("%d", &third);
+        printf("Root [base %.17lf] of %.17lf ", n, a);
+        printf("after %d iterations  = %.17lf\n", third, root_iter(a, n, third));
+        printf("Same calucation with standard root = %.17lf", pow(a, 1 / n));
+        return 0;
+    }
 }
